c8cc0e7ee01b28117570af89ba9a2f47f5f37560
delafuentej <jfuentelucas@gmail.com>
2024-05-15 13:23:45 +0200

    issue mongoDB connection fixed

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const typeDefs = require('../src/graphql/shema');
 const resolvers = require('../src/resolvers/resolvers');
-const {connectionDB} = require('../src/mongodb/dbConnection')
+const  connectToDatabase  = require('../src/mongodb/dbConnection');
 
+console.log('connectiontoDB',connectToDatabase)
 //import library dotenv
 require('dotenv').config({path:'.env.local'});
 
@@ -21,12 +22,32 @@ const startServer = async()=>{
     server.applyMiddleware({app});
 
     //connection to MongoDB
-    await connectionDB;
+    //await connectToDatabase();
 
     const PORT = process.env.PORT || 4000;
-    console.log(PORT)
+    //console.log(PORT)
+    const DATABASE_NAME = process.env.DATABASE_NAME;
+    const COLLECTION_NAME = process.env.COLLECTION_NAME;
+    // console.log('db_name', DATABASE_NAME);
+    // console.log('collection_name', COLLECTION_NAME);
 
-
+    //connection to MongoDB
+    app.get('/', async(req, res)=>{
+        try {
+            const client = await connectToDatabase();
+            //console.log('client', client)
+            const database = client.db(DATABASE_NAME);
+            //console.log('database', database)
+            const collection = database.collection(COLLECTION_NAME);
+            //console.log('collection', collection)
+            const data = await collection.find({}).toArray();
+            //console.log('data', data)
+            res.json(data);
+        }catch(error){
+            console.log(error, error.message);
+            res.status(500).send('Error connecting to db', error.message);
+        }
+    });
 
     app.listen(PORT, ()=>{
         console.log(`Server listening on PORT: ${PORT}${server.graphqlPath}`)
--- server/src/mongodb/dbConnection.js
+++ server/src/mongodb/dbConnection.js
@@ -1,21 +1,69 @@
-const {mongoose} = require('mongoose');
+//const {mongoose} = require('mongoose');
+const { MongoClient } = require('mongodb');
+
 require('dotenv').config({path:'.env.local'});
 
 const uri = process.env.MONGODB_URI;
-console.log(uri);
+console.log('uri',typeof uri);
 
-const connectionDB= async()=>{
-    try{
-        await mongoose.connect(uri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          });
-          console.log('Connected to MongoDB Atlas');
-    }
-    catch(err){
-    console.error('Error connecting to MongoDB', err.message);
-    console.error('Details:', err);
-  };
-};
 
-module.exports = connectionDB;
+let client;
+
+const connectToDatabase=async()=>{
+ if(client){
+  return client;
+ }
+
+try {
+  client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  console.log('client',client)
+  console.log('Connected successfully to MongoDB Atlas');
+  return client;
+} catch (error){
+  console.error('MongoDB connection error', error);
+  throw error;
+}
+
+}
+console.log('connectToDatabase', connectToDatabase)
+
+module.exports = connectToDatabase;
+
+// console.log('client',client)
+// async function run(){
+//   try {
+//     //connect the client to the server
+//     await client.connnect();
+//     // send a ping to confirm a succesful connection
+//     await client.db('admin').command({ping:1});
+
+//     console.log('Pinged your deployment. You successfully connected to MongoDB!');
+//   } finally {
+//     await client.close()
+//   }
+// }
+
+// run().catch(console.dir);
+
+
+
+// const connectionDB= async()=>{
+//   try{
+//       await mongoose.connect(`${uri}`, {
+//           useNewUrlParser: true,
+//           useUnifiedTopology: true,
+//         });
+//         console.log('Connected to MongoDB Atlas');
+//   }
+//   catch(err){npm 
+//   console.error('Error connecting to MongoDB', err.message);
+//   console.error('Details:', err);
+// };
+// };
+// console.log('connectionDB dbConnection.js',connectionDB)
+
+// module.exports = connectionDB();
