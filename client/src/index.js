8271070cd9757e8c87a7a657330d6d578bd7ed7c
delafuentej <jfuentelucas@gmail.com>
2024-05-18 12:09:03 +0200

    apollo client configured

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//ToastContainer =>notifications package
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { createHttpLink, InMemoryCache, ApolloClient, ApolloProvider} from '@apollo/client';
+
+const URI= process.env.URI;
+// link to graphql server
+const link= createHttpLink({
+  uri: `${URI}`
+});
+const cache = new InMemoryCache();
+//to create ApolloClient
+const client = new ApolloClient({
+  link, 
+  cache
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+            <App />
+            <ToastContainer />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
