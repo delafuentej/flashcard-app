75f85582119acb12290a49f198ff99793fb16c3a
delafuentej <jfuentelucas@gmail.com>
2024-05-21 10:27:39 +0200

    layout component added to include the navbar component + child components with outlet

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,18 @@ import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} fr
 import AllFlashcards from '../src/Components/views/AllFlashcards/AllFlashcards.js';
 import AddFlashcard from '../src/Components/views/AddFlashcard/AddFlashcard.js';
 import EditFlashcard from '../src/Components/views/EditFlashcard/EditFlashcard.js';
-import Navbar from './Components/Navbar/Navbar.js';
+import Layout from './Components/Layout/Layout.js';
 
 import './App.css';
 
 
 
 const appRouter= createBrowserRouter(createRoutesFromElements(
-  <>
-                <Route path='/' element={<AllFlashcards />} />
+          <Route path="/" element={<Layout />}>
+                <Route index  element={<AllFlashcards />} />
                 <Route path='add-flashcard' element={<AddFlashcard />} />
                 <Route path= 'edit-flashcard/:id' element={<EditFlashcard />} />
-              
-  </>           
+          </Route>          
               
 ));
 
@@ -22,9 +21,9 @@ const appRouter= createBrowserRouter(createRoutesFromElements(
 function App() {
   return (
     <>
-    
+  
     <RouterProvider router={appRouter}>
-      <Navbar />
+     
     </RouterProvider>
   
   
