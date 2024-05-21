05cd3c94cf6525c5cbe9a397b9bc2ed1854a9877
delafuentej <jfuentelucas@gmail.com>
2024-05-21 11:39:20 +0200

    card-header structur with 4 buttons to update, to chek, to trash, to edit

--- client/src/Components/Flashcard/Flashcard.js
+++ client/src/Components/Flashcard/Flashcard.js
@@ -1,9 +1,75 @@
 
+import React, {useState} from 'react';
+
 const Flashcard=()=>{
+    //state flashcard:
+    const[showAnswer, setShowAnswer] = useState(false);
     return (
+        // card-header structure with four buttons
         <div className='card'>
-            Card
+            <div className='card-header'>
+                <div className='level'>
+                    <div className='level-left'>
+                        <div classNamew='level-item p-2'>
+                            <div className='buttons'>
+                                <button className='button'> 
+                                    <span className='icon is-small'>
+                                        <i className='fas fa-sync-alt' />
+                                    </span>
+
+                                </button>
+
+                                <button className='button'>
+                                    <span className='icon is-small'>
+                                            <i className='fas fa-check' />
+                                    </span>
+                                    
+                                </button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className='level-right'>               
+                        <div classNamew='level-item p-2'>
+                                <div className='buttons'>
+                                    <button className='button'> 
+                                        <span className='icon is-small has-text-dark'>
+                                            <i className='fas fa-pen' />
+                                        </span>
+
+                                    </button>
+
+                                    <button className='button'>
+                                        <span className='icon is-small'>
+                                                <i className='fas fa-trash' />
+                                        </span>
+                                        
+                                    </button>
+
+                                </div>
+
+                            </div>
+
+                    </div>
+
+
+
+
+
+
+
+
+                </div>
+
+            </div>
+
         </div>
+
+         
+        
     )
 
 };
