7a917a659ddc170d92ca8d77949ea60f325627f0
delafuentej <jfuentelucas@gmail.com>
2024-05-21 12:35:52 +0200

    component addflashcard structured

--- client/src/Components/views/AddFlashcard/AddFlashcard.js
+++ client/src/Components/views/AddFlashcard/AddFlashcard.js
@@ -1,8 +1,61 @@
+import React, {useState} from 'react';
 import styles from './AddFlasscard.module.css';
 
 const AddFlashcard =()=>{
+    // state component:
+    const [ question, setQuestion ] = useState('');
+    const [ answer, setAnswer] = useState('');
     return(
-        <h1>AddFlashcard</h1>
+        <div className='container'>
+             <h1 className='title'>AddF lashcard</h1>
+
+             <div className='box'>
+                <form>
+                    <div className='field'>
+                        <label className='label' htmlFor='question'>Question</label>
+                        <div className='control'>
+                            <input
+                                className='input'
+                                type='text'
+                                name='question'
+                                id='question'
+                                required
+                            />
+
+                        </div>
+
+                    </div>
+
+                    <div className='field'>
+                        <label className='label' htmlFor='answer'>Answer</label>
+                        <div className='control'>
+                            <textarea
+                                className='textarea'
+                                name='answer'
+                                id='answer'
+                                required
+                            />
+
+                        </div>
+
+                    </div>
+
+                    <div className="field">
+                        <div className="control">
+                            <button className="button is-link" type="submit">
+                            Add Flashcard
+                            </button>
+                        </div>
+                    </div>
+
+
+
+
+                </form>
+
+             </div>
+        </div>
+       
     )
 }
 
