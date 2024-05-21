93906b0e55275b6cd13b627ce904ab6d382b150d
delafuentej <jfuentelucas@gmail.com>
2024-05-21 12:18:12 +0200

    allflashcards component structured

--- client/src/Components/views/AllFlashcards/AllFlashcards.js
+++ client/src/Components/views/AllFlashcards/AllFlashcards.js
@@ -1,8 +1,24 @@
 import styles from './AllFlashcards.module.css';
+import Flashcard from '../../Flashcard/Flashcard';
 
 const AllFlashcards =()=>{
     return(
-        <h1>AllFlashcards</h1>
+        <div className='container'>
+            <h1> All Flashcards
+                {/* button to reset flashcards */}
+                <button className='button is-info' type='button'>
+                    Reset Flashcards
+                </button>
+            </h1>
+            <div className='box has-background-info'>
+                <div className='columns is-multiline'>
+                        {/* Flashcards */}
+
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
